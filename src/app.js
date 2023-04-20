fc7b8bc383f4ee78d424b349b7dc4e3bfbd2e594
tessahenley <124536938+tessahenley@users.noreply.github.com>
2023-04-20 18:01:51 -0400

    Updated weather icon

--- src/app.js
+++ src/app.js
@@ -32,11 +32,15 @@ descriptionElement.innerHTML = response.data.condition.description;
 let humidityElement = document.querySelector("#humidity");
 humidityElement.innerHTML = "Humidity: " + Math.round(response.data.temperature.humidity) + "%";
 let windElement = document.querySelector("#wind");
-windElement.innerHTML = "Wind: " + Math.round(response.data.wind.speed) + "mph";
+windElement.innerHTML = "Wind: " + Math.round(response.data.wind.speed) + " mph";
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML = formatDate(response.data.time * 1000);
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+iconElement.setAttribute("alt", response.data.condition.description);
 }
 let apiKey = "f5463cfa1b8a4atd0o0764e3fff9eb42";
-let apiUrl="https://api.shecodes.io/weather/v1/current?query=New York&key=f5463cfa1b8a4atd0o0764e3fff9eb42&units=imperial";
+let city = "Orlando";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
