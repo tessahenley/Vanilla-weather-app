3b86a9b5e24d15551af24a61e39204a1780da1cc
tessahenley <124536938+tessahenley@users.noreply.github.com>
2023-04-20 17:19:07 -0400

    Updated the Time and Date

--- src/app.js
+++ src/app.js
@@ -2,7 +2,22 @@ function formatDate(timestamp){
     let date = new Date(timestamp);
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let day = date.getDay();
+    if (minutes < 10 ){
+        minutes = `0${minutes}`;
+    }
+    if (hours < 10 ){
+        hours = `0${hours}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
 
